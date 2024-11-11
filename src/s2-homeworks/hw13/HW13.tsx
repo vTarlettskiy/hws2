ef2d171f28d62dca3414b102365542123391d1b2
vTarlettskiy <v.tarlettskiy@gmail.com>
2024-11-11 19:44:11 +0300

    hm1 sprint5

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -36,7 +36,7 @@ const HW13 = () => {
             .then((res) => {
                 setCode('Код 200!')
                 setImage(success200)
-                setText('...все ок)')
+                setText('...всё ок)')
                 setInfo('код 200 - обычно означает, что скорее всего все ОК)')
                 // дописать
 
